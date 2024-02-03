43a6ebce0b6583d33984e1d5f0d9b8622b0f1abc
oNePlay <carnificem1993@gmail.com>
2024-02-03 08:41:58 +0300

    готовое ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,83 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //задача 1
+        System.out.println("задание 1");
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        //задача 2
+        System.out.println("задание 2");
+        clientOS = 0;
+        int clientDeviceYear = 2011;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        //задание 3
+        System.out.println("задание 3");
+        int year = 1992;
+        if ((year > 1584) && (year % 4 == 0) || (year / 400 == 0) && (year % 100 != 0)) {
+            System.out.println(year + " является высокосным");
+        } else {
+            System.out.println(year + " не является высокосным");
+        }
+        //задача 4
+        System.out.println("задание 4");
+        int diliveryDistance = 100;
+        if ((diliveryDistance > 0) && (diliveryDistance <= 20)) {
+            System.out.println("потребуется один день на доставку ");
+        }
+        if ((diliveryDistance > 20) && (diliveryDistance <= 60)) {
+            System.out.println("на доставку потребуется два дня");
+        }
+        if ((diliveryDistance > 60) && (diliveryDistance <= 100)) {
+            System.out.println("на досьтавку потребуется три дня");
+        }
+        //задача 5
+        System.out.println("задание 5");
+        int monthNumber = 3;
+        switch (monthNumber) {
+            case 12, 1, 2:
+                System.out.println("Зимний сезон.");
+                break;
+            case 3, 4, 5:
+                System.out.println("Весенний сезон.");
+                break;
+            case 6, 7, 8:
+                System.out.println("Летний сезон.");
+                break;
+            case 9, 10, 11:
+                System.out.println("Осенний сезон.");
+                break;
+            default:
+                System.out.println("Такого месяца не сушествует.");
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
